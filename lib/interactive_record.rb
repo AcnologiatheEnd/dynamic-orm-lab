8b766123ee118ec8d6d503ca39bd09ca343a57a3
Winston  Chung <18.chung.winston@ctkprep.org>
2019-11-27 06:48:47 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -60,14 +60,6 @@ class InteractiveRecord
     
   end
   
-  def self.find_by(attribute)
-    column = attribute.to_a.join(', ').split(', ')
-    sql = "SELECT * FROM #{self.table_name} WHERE #{column[1]} = '#{column[0]}'"
-    
-    DB[:conn].execute(sql)
-    
-  end  
-  
   def self.find_by(attr)
     column = attr.to_a.join(', ').split(', ')
        sql = "SELECT * FROM #{self.table_name} WHERE #{column[0]} = '#{column[1]}' "
